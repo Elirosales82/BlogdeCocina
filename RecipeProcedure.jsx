451354c9b4aa4c6ab3bc8586a3ca90c2fc191f2c
Elirosales82 <128557559+Elirosales82@users.noreply.github.com>
2024-04-25 19:11:53 -0300

    Add files via upload

---
+++ RecipeProcedure.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RecipeProcedure () {
+    return (
+        <p>
+            Procedimiento
+        </p>
+    )
+
+
+}
---
+++ RecipeTitle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RecipeTitle = () => {
+    return (
+        <h2>HUEVOS FRITOS</h2>
+    )
+}
+
+export default RecipeTitle;
